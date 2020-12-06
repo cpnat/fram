5145292a595101465feec3c4e77e1114fc3e158c
cpnat <cpnattrass@googlemail.com>
2020-12-06 15:23:35 +0100

    Minor cleanup of launch script

--- fram/main.py
+++ fram/main.py
@@ -1,14 +1,22 @@
-import sys
-import os
+import argparse, sys
 from fram.pipelines.bigquery_pipeline import BigQueryPipeline
 
 if __name__ == "__main__":
 
-    gcloud_project = sys.argv[1]
-    neo4j_endpoint = sys.argv[2]
-    neo4j_user = sys.argv[3]
-    neo4j_password = sys.argv[4]
-    temp_folder_path = sys.argv[5]
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--gcloud_project', help='GCP project from which to obtain Bigquery metadata')
+    parser.add_argument('--neo4j_endpoint', help='Address of Neo4j HTTP enpoint')
+    parser.add_argument('--neo4j_user', help='Neo4j username')
+    parser.add_argument('--neo4j_password', help='Neo4j password')
+    parser.add_argument('--temp_folder_path', help='Staging location for temporary files')
+    args = parser.parse_args()
 
-    pipeline = BigQueryPipeline(gcloud_project, neo4j_endpoint, neo4j_user, neo4j_password, temp_folder_path)
+    pipeline = BigQueryPipeline(
+        gcloud_project=args.gcloud_project,
+        neo4j_endpoint=args.neo4j_endpoint,
+        neo4j_user=args.neo4j_user,
+        neo4j_password=args.neo4j_password,
+        temp_folder_path=args.temp_folder_path
+    )
+
     pipeline.run()
